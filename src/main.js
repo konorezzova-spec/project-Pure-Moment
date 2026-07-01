b7ecfb8f9059900baebbce5358ff1c30d98c5c7a
konorezzova-spec <konorezzova@gmail.com>
2026-07-01 09:10:47 +0300

    added fonts and variables

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './js/header.js';
 import './js/hero.js';
 import './js/feedbacks.js';
-import './js/portfolio.js'
-import './js/faq.js'
-import './js/contacts.js'
+import './js/portfolio.js';
+import './js/faq.js';
+import './js/contacts.js';
