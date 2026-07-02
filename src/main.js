f20ab69f24527dca5ef4953608c09640604e5337
konorezzova-spec <konorezzova@gmail.com>
2026-07-02 09:33:44 +0300

    fixed paths in main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import './js/header.js';
-import './js/feedbacks.js';
-import './js/portfolio.js';
-import './js/faq.js';
-import './js/contacts.js';
-import './js/success_modal.js';
+import './js/header/header.js';
+import './js/feedbacks/feedbacks.js';
+import './js/portfolio/portfolio.js';
+import './js/faq/faq.js';
+import './js/contacts/contacts.js';
+import './js/success_modal/success_modal.js';
