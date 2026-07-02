ce8b62b9144b4963f207078ee982f9f75223ebab
Anastasiia Kalyniuk <Kalyniuk.an@gmail.com>
2026-07-02 19:32:38 +0300

    init gallery portfolio

---
+++ src/js/portfolio/portfolio-api.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export async function getPortfolioItemsCategories() {
+ 
+  const URL = "https://wedding-photographer.b.goit.study/api/categories";
+  return (await axios.get(URL)).data;
+};
+
+export async function getPortfolioGallery(page = 1, limit = 3, categoryId='') {
+  const serchParams = new URLSearchParams({
+    page: page,
+    limit: limit,
+  });
+
+  if (categoryId) {
+    serchParams.append('categoryId', categoryId);
+  }
+  const URL = `https://wedding-photographer.b.goit.study/api/wedding-photos/`;
+  
+  return await (await axios.get(URL, {params: serchParams})).data;
+}
---
+++ src/js/portfolio/portfolio-render.js
@@ -0,0 +1,43 @@
+const categories = document.querySelector('.portfolio-categories');
+const gallery = document.querySelector('.portfolio-gallery');
+
+const loadMoreButton = document.querySelector('.portfolio-load-more');
+const loader = document.querySelector('.loader');
+
+export function createCategoryButtons(categoriesData) {
+  const markup = categoriesData.map(category => {
+    return `<li><button class="portfolio_category_btn" data-id="${category._id}">${category.category}</button></li>`;
+  });
+  categories.insertAdjacentHTML('beforeend', markup.join(''));
+}
+
+function createGalleryItems({ img, title }) {
+  return `<li class="portfolio_gallery_item"><a href="${img}"><img src="${img}" alt="${title}"></a></li>`;
+ }
+
+export function createGallery(itemsData) {
+  const markup = itemsData.map(item => {
+    return createGalleryItems(item);
+  });
+  gallery.insertAdjacentHTML('beforeend', markup.join(''));
+}
+
+export function clearGallery() {
+  gallery.innerHTML = '';
+}
+
+export function showLoader() {
+  loader.classList.remove("hidden");
+}
+
+export function hideLoader() {
+  loader.classList.add("hidden");
+}
+
+export function showLoadMoreBtn() {
+  loadMoreButton.classList.remove('hidden');
+}
+
+export function hideLoadMoreBtn() {
+  loadMoreButton.classList.add('hidden');
+}
